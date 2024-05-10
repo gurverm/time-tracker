e1bdc5f59f17db5d1544d7566af5f6060c6ff1bf
Gurveer Madurai <maduraigurveer@gmail.com>
2024-05-09 22:16:31 -0700

    EntryPage.js file renamed and updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
-// import AddEntry from "./components/TimeEntries/AddEntry";
-// import EntryList from "./components/TimeEntries/EntryList";
 import HomePage from "./components/HomePage/HomePage";
-import EntryPage from "./pages/entryPage";
+import EntryPage from "./pages/EntryPage";
 
 function App() {
   return (
@@ -14,8 +12,6 @@ function App() {
         <Route path="/" element={<HomePage/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
-        {/* <Route path="/add-entry" element={<AddEntry/>} />
-        <Route path="/entries" element={<EntryList/>} /> */}
         <Route path="/entry" element={<EntryPage/>} />
       </Routes>
     </Router>
